4c5293a5fcfaa82b373f92577e9845130a8cab20
Montival <montival_junior@yahoo.com.br>
2020-09-08 17:07:35 -0300

    Inserido pre carregamento de usuario

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,4 +6,4 @@ INSERT INTO candidato (nome, partido, numero) VALUES ('Marina Silva', 'PSB', 40)
 INSERT INTO candidato (nome, partido, numero) VALUES ('Pastor Everaldo', 'PSC', 20);
 INSERT INTO candidato (nome, partido, numero) VALUES ('Luciana Genro', 'PSOL', 50)
 
-
+INSERT INTO users (login, password, admin) VALUES ('month', '$2a$10$Lt/sT0AU.SoNLjlC1thmw.dN8yrHVtokXSHxPJeeUaY6.bfXKEg9i', true);
