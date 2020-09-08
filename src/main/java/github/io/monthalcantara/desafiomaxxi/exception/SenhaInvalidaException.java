598b7c5d26727d7331b9220bc8609b8cfba025fa
Montival <montival_junior@yahoo.com.br>
2020-09-08 17:09:09 -0300

    Criada exception para Senha invalida

---
+++ src/main/java/github/io/monthalcantara/desafiomaxxi/exception/SenhaInvalidaException.java
@@ -0,0 +1,7 @@
+package github.io.monthalcantara.desafiomaxxi.exception;
+
+public class SenhaInvalidaException extends RuntimeException {
+    public SenhaInvalidaException(){
+        super("Senha Inválida");
+    }
+}
