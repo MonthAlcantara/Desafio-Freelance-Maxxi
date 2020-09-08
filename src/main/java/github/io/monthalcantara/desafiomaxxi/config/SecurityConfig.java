0797158367d930efbdc84a881b10252482b0c979
Montival <montival_junior@yahoo.com.br>
2020-09-08 17:05:48 -0300

    Configuracao de seguranca e jwt

---
+++ src/main/java/github/io/monthalcantara/desafiomaxxi/config/SecurityConfig.java
@@ -0,0 +1,70 @@
+package github.io.monthalcantara.desafiomaxxi.config;
+
+import github.io.monthalcantara.desafiomaxxi.jwt.JwtAuthFilter;
+import github.io.monthalcantara.desafiomaxxi.jwt.JwtService;
+import github.io.monthalcantara.desafiomaxxi.service.implementacoes.UsuarioServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UsuarioServiceImpl userService;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public OncePerRequestFilter jwtFilter(){
+        return new JwtAuthFilter(jwtService, userService);
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+                .passwordEncoder(passwordEncoder());
+    }
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/h2/**",
+                "/v1/usuarios/auth",
+                "/configuration/**"
+        );
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/v1/usuarios/**")
+                .hasAnyRole("USER", "ADMIN")
+                .antMatchers("/v1/usuarios/**",
+                        "/v1/session/**")
+                .hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST,"/v1/usuarios/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+}
---
+++ src/main/java/github/io/monthalcantara/desafiomaxxi/dto/TokenDTO.java
@@ -0,0 +1,22 @@
+package github.io.monthalcantara.desafiomaxxi.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TokenDTO  implements Serializable {
+
+    private String login;
+
+    private String token;
+
+
+}
