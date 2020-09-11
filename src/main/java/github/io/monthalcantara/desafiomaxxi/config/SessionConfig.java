69f104921aa6eadd0d08703899e24a42e8a1519f
Montival <montival_junior@yahoo.com.br>
2020-09-11 13:52:29 -0300

    Adicionado tempo de expiraçao de sessao por inatividade

--- src/main/java/github/io/monthalcantara/desafiomaxxi/config/SessionConfig.java
+++ src/main/java/github/io/monthalcantara/desafiomaxxi/config/SessionConfig.java
@@ -1,12 +1,16 @@
 package github.io.monthalcantara.desafiomaxxi.config;
 
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-
-@EnableRedisHttpSession
+@Configuration
+@EnableCaching
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 600)
 public class SessionConfig {
+
     @Bean
     public LettuceConnectionFactory connectionFactory() {
         return new LettuceConnectionFactory();
