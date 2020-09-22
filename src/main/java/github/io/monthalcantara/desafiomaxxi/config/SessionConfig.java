efb179c97f8e7b0fec33b04773b031c475dbbfc8
montival_junior@yahoo.com.br <Europa2017!>
2020-09-22 13:16:05 -0300

    Adicionada classe configuração para redis

--- src/main/java/github/io/monthalcantara/desafiomaxxi/config/SessionConfig.java
+++ src/main/java/github/io/monthalcantara/desafiomaxxi/config/SessionConfig.java
@@ -4,6 +4,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
@@ -14,5 +15,12 @@ public class SessionConfig {
     @Bean
     public LettuceConnectionFactory connectionFactory() {
         return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    RedisTemplate redisTemplate(){
+        RedisTemplate redisTemplate=new RedisTemplate();
+        redisTemplate.setConnectionFactory(connectionFactory());
+        return redisTemplate;
     }
 }
